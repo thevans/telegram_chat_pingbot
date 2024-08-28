8641375ed3154372381167c4812c1f9212a29cb7
thevans.vilella <thevans.vilella@gmail.com>
2024-08-28 11:31:41 -0300

    Alteração do script para possibilitar agendamento de mensagem para o próximo dia

--- ping_bot.py
+++ ping_bot.py
@@ -1,45 +1,78 @@
-from telethon.sync import TelegramClient, events
-import time
+from telethon.sync import TelegramClient
 import asyncio
+import argparse
+import datetime
+import logging
 
-api_id = 1234
-api_hash = 'xxxxx'
-
+# Configurações do script
+api_id = 1234  # Seu ID da API do Telegram
+api_hash = 'xxxxx'  # Seu hash secreto da API do Telegram
+phone_number = "+55...."
 debug_group = -11111111111111
 free_group = -11111111111111
 
-interval = 1800
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - Scrtipt de agendamento de mensagems do grupo free - %(levelname)s - %('
+                           'message)s',
+                    datefmt='%d-%m-%Y %H:%M:%S')
 
-with TelegramClient('name', api_id, api_hash) as client:
-   def send_live_message():
-      client.send_message(debug_group, 'Enviando comando /live...')
-      client.send_message(free_group, '/live')
-      time.sleep(60)
-      client.send_message(debug_group, 'Reenviando comando /live...')
-      client.send_message(free_group, '/live')
+client = TelegramClient(f'session_name_ping_script', api_id, api_hash)
 
 
-   def send_tiktok_message():
-      client.send_message(debug_group, 'Enviando comando /tiktok...')
-      client.send_message(free_group, '/tiktok')
-      time.sleep(60)
-      client.send_message(debug_group, 'Reenviando comando /tiktok...')
-      client.send_message(free_group, '/tiktok')
+async def send_live_message(time_for_message):
+    schedule_date = datetime.datetime.fromtimestamp(time_for_message)
+    logging.info(f'Agendando comando /live para {schedule_date}')
+    await client.send_message(free_group, '/live', schedule=schedule_date)
 
 
-   def execute_interval(interval):
-      print(f"Executando a função a cada {interval} segundos.")
-      alternate = False
-      while True:
-         if alternate:
-            send_live_message()
-         else:
-            send_tiktok_message()
-
-         alternate = not alternate
-         # await asyncio.sleep(interval)
-         time.sleep(interval)
+async def send_tiktok_message(time_for_message):
+    schedule_date = datetime.datetime.fromtimestamp(time_for_message)
+    logging.info(f'Agendando comando /live para {schedule_date}')
+    await client.send_message(free_group, '/tiktok', schedule=schedule_date)
 
 
-   asyncio.run(execute_interval(interval))
-   client.run_until_disconnected()
+async def execute_interval(interval):
+    logging.info(f"Executando o agendamento das mensagens a cada {int(interval/60)} minutos...")
+    alternate = False
+    count = 0
+    day_duration = 24 * 60 * 60
+    total_interactions = day_duration / interval
+
+    now = datetime.datetime.now()
+    tomorrow = now + datetime.timedelta(days=1)
+    initial_timestamp = datetime.datetime(tomorrow.year, tomorrow.month, tomorrow.day, 0, 0, 0)
+    seconds_initial_timestamp = int(initial_timestamp.timestamp())
+
+    while total_interactions > count:
+        time_for_message = seconds_initial_timestamp + (count * interval)
+
+        if alternate:
+            await send_live_message(time_for_message)
+        else:
+            await send_tiktok_message(time_for_message)
+
+        alternate = not alternate
+        count += 1
+
+    logging.info(f"Agendamento concluído")
+
+async def main():
+    """Função principal do script."""
+    await client.connect()
+    if not await client.is_user_authorized():
+        await client.send_code_request(phone_number)
+        await client.sign_in(phone_number, input('Enter the code: '))
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--intervalo", type=int, default=15, help="intervalo entre as mensagems em minutos.")
+    args = parser.parse_args()
+
+    interval = args.intervalo * 60
+
+    logging.info("Conectado à conta do Telegram.")
+    await execute_interval(interval)
+    await client.disconnect()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
